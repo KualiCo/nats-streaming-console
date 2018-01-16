5255fa5ae0aefef73999b1fb59c4ed313a153f27
Thomas A. Valletta <tom@vallettas.com>
2018-01-16 10:59:01 -0700

    do not proxy

--- server/routes/server.js
+++ server/routes/server.js
@@ -8,14 +8,23 @@ exports.getServerOptions = async (req, res) => {
 exports.setServerOptions = async (req, res) => {
   const { host, port, monitoringPort } = req.body
   try {
-    const data = await axios
-      .get(`http://${host}:${monitoringPort}/streaming/serverz`)
-      .then(resp => resp.data)
-    options.server = `nats://${host}:${port}`,
-    options.monitor = `http://${host}:${monitoringPort}`,
-    options.cluster = data.cluster_id
-    res.status(200).send({ options, data })
+    const resp = await axios({
+      method: 'get',
+      baseURL: `http://${host}:${monitoringPort}/`,
+      url: '/streaming/serverz',
+      headers: { 'Accept': 'application/json' },
+      proxy: false
+    })
+    updateOptions(resp.data, host, port, monitoringPort)
+    res.status(200).send({ options, data: resp.data })
   } catch (err) {
+    console.log({ err })
     res.status(500).send({ status: 'error' })
   }
+}
+
+function updateOptions(data, host, port, monitoringPort) {
+  options.server = `nats://${host}:${port}`,
+  options.monitor = `http://${host}:${monitoringPort}`,
+  options.cluster = data.cluster_id
 }
