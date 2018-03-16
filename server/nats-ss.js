500d51b202b47e63a7a34f8e57039d22f8a783bb
Sean Schade <sean.schade@myhomepay.com>
2018-03-16 13:55:25 -0500

    added default values back

--- server/nats-ss.js
+++ server/nats-ss.js
@@ -1,9 +1,9 @@
 const { getInstance: getNerveInstance } = require('nats-nerve')
 
 const defaults = {
-  server: process.env.STAN_URL,
-  monitor: process.env.STAN_MONITOR_URL,
-  cluster: process.env.STAN_CLUSTER,
+  server: process.env.STAN_URL || 'nats://localhost:4222',
+  monitor: process.env.STAN_MONITOR_URL || 'http://localhost:8222',
+  cluster: process.env.STAN_CLUSTER || 'test-cluster',
   appName: 'nats-streaming-console'
 }
 
