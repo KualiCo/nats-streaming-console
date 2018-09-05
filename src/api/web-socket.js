22b6dab9c19b5966f4716db4cdd5bf34e1ba20ba
Klaus Jørgensen <klaus@pnorental.com>
2018-09-05 11:21:50 +0200

    connect to host relative uri

--- src/api/web-socket.js
+++ src/api/web-socket.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client'
-const socket = openSocket('http://localhost:8282')
+const socket = openSocket('/')
 
 export const subscribeToChannel = (channel, opts, cb) => {
   socket.on(channel, message => cb(message))
